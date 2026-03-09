965237e600cf24af65ae92bbaa63ccf1e70ab5b8
Anou pro <anou.webu@gmail.com>
2026-03-10 00:11:45 +0100

    ✨ feat: add main static layout with header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      
+        <Header />
+        <main className="grow">
+
+        </main>
+        <Footer />
+      
+    </div>
+  );
 }
 export default App;
