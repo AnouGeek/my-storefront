af543844402dfb7c5b952ec958de61ff3d979b44
Anou pro <anou.webu@gmail.com>
2026-03-11 23:21:16 +0100

    ✨ feat(ui): create product card and responsive grid

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,16 @@
+export default function ProductCard({ product }) {
+  return (
+    <div className="flex flex-col bg-gray-100 rounded-lg shadow-sm p-4 border">
+      <img
+        className="h-48 object-contain"
+        src={product.image}
+        alt={product.title}
+      />
+      <span className="truncate">{product.title}</span>
+      <span>{product.price}</span>
+      <button className="bg-blue-600 hover:bg-blue-500 text-gray-50 py-2 rounded-xl mt-auto">
+        Add to cart
+      </button>
+    </div>
+  );
+}
