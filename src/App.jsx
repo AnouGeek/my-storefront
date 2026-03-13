baf7875f6144953677609b7d77fd63fb66f10d08
Anou pro <anou.webu@gmail.com>
2026-03-13 03:29:59 +0100

    ✨ feat(cart): update ui to display quantities and calculate accurate totals, change cart style

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,19 @@ function App() {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart([...cart, product]);
+    const existItem = cart.find((item) => item.id === product.id);
+
+    if (existItem) {
+      setCart(
+        cart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        ),
+      );
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
   };
 
   return (
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,27 +1,35 @@
 export default function Cart({ cart }) {
   const total = cart.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.price,
+    (accumulator, currentValue) =>
+      accumulator + currentValue.price * currentValue.quantity,
     0,
   );
 
   return (
-    <div className="p-4">
+    <div className="mt-10 p-4 max-w-3xl mx-auto">
       {cart.length === 0
         ? "Your cart is empty"
         : cart.map((product) => (
             <div
-              className="flex items-center border-b py-4 justify-between"
+              className="flex items-center border-b py-4 gap-4"
               key={product.id}
             >
               <img
-                className="h-12 w-12 object-contain"
+                className="h-12 w-12 object-contain shrink-0"
                 src={product.image}
               ></img>
-              <span>{product.title}</span>
-              <span>{product.price}</span>
+              <span className="flex-1 font-semibold truncate">
+                {product.title}
+              </span>
+              <span className="text-green-500 w-12">x{product.quantity}</span>
+              <span className="whitespace-nowrap font-bold w-16">
+                {product.price} €
+              </span>
             </div>
           ))}
-      {total.toFixed(2)}
+      <div className="mt-8 border w-fit p-2 ml-auto rounded-2xl bg-blue-300">
+        {total.toFixed(2)} €
+      </div>
     </div>
   );
 }
